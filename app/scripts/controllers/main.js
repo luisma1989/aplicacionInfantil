fdb6a05743a1905e57c3bb6b092b80537a49791a
Luis Manuel Fernandez Camacho <luisma_1989@hotmail.com>
2014-09-13 15:00:39 +0200

    aplicación completa con primera versión de intro.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,150 +1,74 @@
 'use strict';
 
+    var introguide = introJs();
 
-var introguide = introJs();
-
-$(document).ready(function() {
-      var startbtn   = $('#startdemotour');
-      
-      introguide.setOptions({
+    $(document).ready(function() {
+        introguide.setOptions({
         steps: [
           {
             element: '.ch-grid',
-            intro: 'TVoy a reventarle la boca a kiko por gilipollas',
+            intro: 'Menú principal de la aplicación',
+            position: 'bottom'
+          },
+          {
+            element: '#primerItem',
+            intro: 'Aquí puedes acceder a la aplicación con tu usuario',
+            position: 'bottom'
+          },
+          {
+            element: '#segundoItem',
+            intro: 'Aquí puedes crear un usuario',
+            position: 'bottom'
+          },
+          {
+            element: '#cuartoItem',
+            intro: 'Sección donde se describe el proyecto',
+            position: 'bottom'
+          },
+          {
+            element: '#quintoItem',
+            intro: 'Formulario de contacto',
             position: 'bottom'
           },
           {
             element: '#primary',
-            intro: 'Click this main logo to view a list of all Hongkiat demos.',
+            intro: 'Menú lateral de navegación por la página principal',
             position: 'left'
           },
           {
             element: '#primeraPagina .contenido-seccion',
-            intro: 'Hover over each title to display a longer description.',
-            position: 'bottom'
+            intro: 'Éste es contenido de cualquier sección de la página principal, en este caso el primer juego',
+            position: 'right'
           },
           {
             element: '.titulo-primera-seccion',
-            intro: 'Click this orange button to view the tutorial article in a new tab.',
+            intro: 'Este título en un enlace a la sección',
             position: 'right'
           }
         ]
       });
-
-      $('#startdemotour').bind('click', function (){
-      });
-      
-
-
-      redrawDotNav();
-
-        $(window).bind('scroll',function(e){
-          parallaxScroll();
-          redrawDotNav();
-        });
-
-      $('a.primeraSeccion').click(function(){
-          $('html, body').animate({
-            scrollTop:0
-          }, 1000, function() {
-            parallaxScroll(); 
-        });
-          return false;
-      });
-
-      $('a.segundaSeccion').click(function(){
-        $('html, body').animate({
-          scrollTop:$('#segundaSeccion').offset().top
-        }, 1000, function() {
-          parallaxScroll();
-      });
-        return false;
-      });
-
-      $('a.terceraSeccion').click(function(){
-        $('html, body').animate({
-          scrollTop:$('#terceraSeccion').offset().top
-        }, 1000, function() {
-          parallaxScroll();
-      });
-        return false;
-      });
-
-      $('a.cuartaSeccion').click(function(){
-        $('html, body').animate({
-          scrollTop:$('#cuartaSeccion').offset().top
-        }, 1000, function() {
-          parallaxScroll();
-      });
-        return false;
-      });
-
-      $('a.quintaSeccion').click(function(){
-        $('html, body').animate({
-          scrollTop:$('#quintaSeccion').offset().top
-        }, 1000, function() {
-          parallaxScroll();
-      });
-        return false;
-      });
-
-        $('nav#primary a').hover(
-          function () {
-          $(this).prev('h1').show();
-        },
-        function () {
-          $(this).prev('h1').hide();
-        }
-        );
     });
 
-    function parallaxScroll(){
-      var scrolled = $(window).scrollTop();
-      $('#parallax-bg1').css('top',(0-(scrolled*.30))+'px');
-      $('#parallax-bg2').css('top',(0-(scrolled*.10))+'px');
-    }
-
-    function redrawDotNav(){
-      var section1Top =  0;
-      var section2Top =  $('#segundaSeccion').offset().top - (($('#terceraSeccion').offset().top - $('#segundaSeccion').offset().top) / 2);
-      var section3Top =  $('#terceraSeccion').offset().top - (($('#cuartaSeccion').offset().top - $('#terceraSeccion').offset().top) / 2);
-      var section4Top =  $('#cuartaSeccion').offset().top - (($(document).height() - $('#cuartaSeccion').offset().top) / 2);
-      var section5Top =  $('#quintaSeccion').offset().top - (($(document).height() - $('#quintaSeccion').offset().top) / 2);
-      $('nav#primary a').removeClass('active');
-      if($(document).scrollTop() >= section1Top && $(document).scrollTop() < section2Top){
-        $('nav#primary a.inicioPagina').addClass('active');
-      } else if ($(document).scrollTop() >= section2Top && $(document).scrollTop() < section3Top){
-        $('nav#primary a.segundaSeccion').addClass('active');
-      } else if ($(document).scrollTop() >= section3Top && $(document).scrollTop() < section4Top){
-        $('nav#primary a.terceraSeccion').addClass('active');
-      } else if ($(document).scrollTop() >= section4Top){
-        $('nav#primary a.cuartaSeccion').addClass('active');
-      } else if ($(document).scrollTop() >= section5Top){
-        $('nav#primary a.quintaSeccion').addClass('active');
-      }
-
-    }
-
-angular.module('aplicacionCompleta2App')
+  angular.module('aplicacionCompleta2App')
 	.controller('MainCtrl', function mainController ($scope, $modal, Auth, $location) {
 
 
-	$scope.empiezaTour = function () {
-		introguide.start();
-	};
+        $scope.empiezaTour = function () {
+            introguide.start();
+        };
 
-    $scope.open = function () {
-	    var ventanaModal = $modal.open({
-	      templateUrl: '/views/partials/ventanaModal.html',
-	      controller: 'VentanaModalCtrl'
-	    });
-    };
+        $scope.open = function () {
+            var ventanaModal = $modal.open({
+              templateUrl: '/views/partials/ventanaModal.html',
+              controller: 'VentanaModalCtrl'
+            });
+        };
 
-	$scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
+        $scope.logout = function() {
+          Auth.logout()
+          .then(function() {
+            $location.path('/login');
+          });
+        };
+
 	});
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,4 +1,6 @@
 'use strict';
+    
+
 
 angular.module('aplicacionCompleta2App')
   .controller('SignupCtrl', function ($scope, Auth, $location) {
